32d7f63211c2414fa2e7d5bcec5a50ad29bd1664
wubiao <wubiao@missfresh.cn>
2018-12-26 10:24:32 +0800

    init

--- src/main/java/com/generator/GeneratorMojoFacade.java
+++ src/main/java/com/generator/GeneratorMojoFacade.java
@@ -1,10 +1,12 @@
 package com.generator;
 
+import com.generator.config.ApplicationConfig;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugin.logging.Log;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 
 @Mojo(name = "gen")
@@ -14,7 +16,7 @@ public class GeneratorMojoFacade extends AbstractMojo {
 
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
-
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(ApplicationConfig.class);
 
 
     }
---
+++ src/main/java/com/generator/config/ApplicationConfig.java
@@ -0,0 +1,17 @@
+package com.generator.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+/**
+ * @author wubiao
+ */
+@EnableAspectJAutoProxy
+@Configuration
+public class ApplicationConfig {
+	 
+
+}
+
