8e4186b85d3341238c87c41109d983850c9204c6
wubiao <wubiao@missfresh.cn>
2018-12-29 23:16:17 +0800

    拖拽排序

--- code_generator_html/src/App.vue
+++ code_generator_html/src/App.vue
@@ -1,14 +1,29 @@
 <template>
   <div>
     <h2>Constrain Draggable</h2>
-    <Panel :bodyStyle="">
-      <template v-for="(item,index) in items">
-        <div v-Draggable="{drag:onDrag,revert:false,dragEnd:onDragEnd,axis:axis}" :row="item" :id="index" :index="index"
-             :style="dragStyle">
-          <p style="text-align:center">Drag Me{{item}}</p>
+    <!--<Panel :bodyStyle="{position: 'relative', width: '200px',height:'100%',border:'1px solid',padding:'2px'}">-->
+    <!--<template v-for="(item,index) in items">-->
+    <!--<div v-Draggable="{drag:onDrag,revert:false,dragEnd:onDragEnd,axis:axis}" :row="item" :id="index" :index="index"-->
+    <!--:style="{border: '1px solid #ccc', float: 'left',width: '100%',height: '100px'}">-->
+    <!--<p style="text-align:center">Drag Me{{item}}</p>-->
+    <!--</div>-->
+    <!--</template>-->
+    <!--</Panel>-->
+
+
+    <DataList style="height:100%"
+              :data="items"
+              itemCls="dataitem">
+      <template slot-scope="{row,rowIndex}">
+        <div v-Draggable="{drag:onDrag,revert:true,dragEnd:onDragEnd,axis:axis}" :index=rowIndex
+             :style="{border: '1px solid #ccc',width: '100%',height: '100px'}">
+          <p style="text-align:center">Drag Me{{row}}{{rowIndex}}</p>
         </div>
       </template>
-    </Panel>
+    </DataList>
+    <!--<DataGrid :data="data" style="height:250px">-->
+    <!--</DataGrid>-->
+
   </div>
 </template>
 
@@ -16,7 +31,7 @@
   export default {
     data() {
       var items = [];
-      for (var i = 0; i < 4; i++) {
+      for (var i = 4; i < 8; i++) {
         items.push({"id": i, "name_": `zhangsan${i}`});
       }
       return {
@@ -48,25 +63,21 @@
     methods: {
       onDragEnd(event) {
         let d = event;
-        console.info(d.left + "," + d.top);
         let top = d.top;
         let startTop = event.startTop;
-        let moveTop = startTop - top;
+        let moveTop = top - startTop;
         if (Math.abs(moveTop) < this.dragHeight / 2) {
-          console.info("小于一半不排序");
+         return
         }
-        // Math.ceil(
-        console.info(moveTop);
-        console.info(this.dragHeight);
         let moveStep = Math.ceil(moveTop / this.dragHeight);
-        console.info(moveStep);
-        let items = this.items;
         let target = event.target.$el.attributes;
-        let cruIndex = target.index;
-        let cruRow = items[cruIndex];
+        let cruIndex = Number(target.index.nodeValue);
+        let cruRow = this.items[cruIndex];
         let insertInde = moveStep + cruIndex;
-        items.splice(cruIndex,1);
-        items.splice(insertInde,0,cruRow);
+        this.items.splice(cruIndex, 1);
+        this.items.splice(insertInde, 0, cruRow);
+        let newItems = [...this.items];
+        this.items.splice(0,newItems.length,...newItems);
       },
       onDrag(event) {
         // console.info(event);
