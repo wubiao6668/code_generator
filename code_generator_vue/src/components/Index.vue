c8000e14ef2f69aa121302f3ed358054e4d4d301
wubiao <wubiao@missfresh.cn>
2019-01-04 18:54:43 +0800

    init setter

--- code_generator_vue/src/components/Index.vue
+++ code_generator_vue/src/components/Index.vue
@@ -1,36 +1,86 @@
 <template>
-  <div>
-    <h2>TreeGrid Template</h2>
-    <TreeGrid style="height:250px"
-              :data="data" :footerData="footerData" :showFooter="true"
-              idField="id" treeField="name">
-      <GridColumn field="name" title="Name" width="30%">
-        <template slot="footer" slot-scope="{row}">
-          <span class="tree-file icon-sum"></span>
-          {{row.name}}
-        </template>
-      </GridColumn>
-      <GridColumn field="persons" title="Persons" align="right"></GridColumn>
-      <GridColumn field="begin" title="Begin Date"></GridColumn>
-      <GridColumn field="end" title="End Date"></GridColumn>
-      <GridColumn field="progress" title="Progress">
-        <template slot="cell" slot-scope="{row}">
-          <div class="progress">
-            <div class="progress-inner" :style="{width:row.progress+'%'}">
-              {{row.progress}} %
+    <div>
+        <h2>TreeGrid Template</h2>
+        <TreeGrid style="height:250px"
+                  :data="data" :footerData="footerData" :showFooter="true"
+                  idField="id" treeField="name">
+            <GridColumn field="name" title="Name" width="30%">
+                <template slot="footer" slot-scope="{row}">
+                    <span class="tree-file icon-sum"></span>
+                    {{row.name}}
+                </template>
+            </GridColumn>
+            <GridColumn field="persons" title="Persons" align="right"></GridColumn>
+            <GridColumn field="begin" title="Begin Date"></GridColumn>
+            <GridColumn field="end" title="End Date"></GridColumn>
+            <GridColumn field="progress" title="Progress">
+                <template slot="cell" slot-scope="{row}">
+                    <div class="progress" v-Draggable="{row:row,revert:true,proxy:$refs.proxy,dragStart:onDragStart}"  :row="row">
+                        <div class="progress-inner" :style="{width:row.progress+'%'}" :row="row">
+                            拖拽
+                        </div>
+                    </div>
+                </template>
+            </GridColumn>
+        </TreeGrid>
+        <div style="margin-top: 10px">
+            <Tabs :tabPosition="position">
+                <TabPanel :title="'生成set方法'">
+                    <p>Tab Panel1 生成set方法</p>
+                    <TreeGrid style="height:250px" v-Droppable="{drop:onDrop}"
+                              :data="setterData" :footerData="footerData" :showFooter="true"
+                              idField="id" treeField="name">
+                        <GridColumn field="name" title="Name" width="30%">
+                            <template slot="footer" slot-scope="{row}">
+                                <span class="tree-file icon-sum"></span>
+                                {{row.name}}
+                            </template>
+                        </GridColumn>
+                        <GridColumn field="persons" title="Persons" align="right"></GridColumn>
+                        <GridColumn field="begin" title="Begin Date"></GridColumn>
+                        <GridColumn field="end" title="End Date"></GridColumn>
+                        <GridColumn field="progress" title="Progress">
+                            <template slot="cell" slot-scope="{row}">
+                                <div class="progress" v-Draggable="{row:row,revert:true,proxy:$refs.proxy}" :row="row">
+                                    <div class="progress-inner" :style="{width:row.progress+'%'}">
+                                        拖拽
+                                    </div>
+                                </div>
+                            </template>
+                        </GridColumn>
+                    </TreeGrid>
+
+                </TabPanel>
+                <TabPanel :title="'生成JSON文档'">
+                    <p>Tab Panel2</p>
+                </TabPanel>
+                <TabPanel :title="'生成wiki文档'">
+                    <p>Tab Panel3</p>
+                </TabPanel>
+                <TabPanel :title="'Help'" :closable="true" iconCls="icon-help">
+                    <p>This is the help content.</p>
+                </TabPanel>
+            </Tabs>
+        </div>
+
+        <DraggableProxy ref="proxy">
+            sdsdsdsd
+            <div v-if="dragItem" class="product product-proxy">
+                <p>sfdsdfs</p>
+                <p>ssdsdsdsdsd</p>
             </div>
-          </div>
-        </template>
-      </GridColumn>
-    </TreeGrid>
-  </div>
+        </DraggableProxy>
+    </div>
 </template>
 
 <script>
     export default {
         data() {
             return {
+                position: "right",
                 data: null,
+                dragItem: null,
+                setterData: [],
                 footerData: null
             };
         },
@@ -44,6 +94,15 @@
             ];
         },
         methods: {
+            onDragStart(state){
+                this.dragItem = state.target.row;
+            },
+            onDrop(scope) {
+                // console.info(scope)
+                // console.info(this)
+                console.info(this.dragItem)
+                this.setterData.push(this.dragItem);
+            },
             sum(data) {
                 let persons = 0;
                 for (let row of data) {
@@ -122,15 +181,16 @@
     };
 </script>
 <style>
-  .progress {
-    width: 100%;
-    height: 20px;
-    border: 1px solid #ccc;
-  }
-  .progress-inner {
-    height: 100%;
-    background: red;
-    color: #fff;
-    padding: 0 4px;
-  }
+    .progress {
+        width: 100%;
+        height: 20px;
+        border: 1px solid #ccc;
+    }
+
+    .progress-inner {
+        height: 100%;
+        background: red;
+        color: #fff;
+        padding: 0 4px;
+    }
 </style>
